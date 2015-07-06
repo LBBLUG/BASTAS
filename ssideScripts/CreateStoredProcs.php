c14cb4b499788524b3330f794060ccc9877bd283
Brian <brian.mcgonagill@tylertech.com>
2015-07-06 15:56:58 -0500

    Added Stored proc php file
    Updated Stored proc and stubbed config.ini.php file.

--- ssideScripts/CreateStoredProcs.php
+++ ssideScripts/CreateStoredProcs.php
@@ -4,7 +4,12 @@
 // This file is to create stored procedures for the BASTAS application
 // ************************************************************************
 
-require("php/src_files/config/config.php");
+$ini_array = parse_ini_file("php/src_files/config/config.ini.php");
+
+$ServerAddress = $ini_array['ServerName'];
+$Username = $ini_array['Username'];
+$Password = $ini_array['Password'];
+$Database = $ini_array['Database'];
 
 $conn = new mysqli($ServerAddress, $Username, $Password, $Database);
 
@@ -16,13 +21,13 @@ if ($conn->connect_error)
     echo "<br /><br />";
 }
 
+$createrecipAndAddress = "CREATE PROCEDURE pop_recipAndAddress(IN lastName varchar(30), IN firstName varchar(30), IN sex varchar(1), IN h_phone varchar(13), IN c_phone varchar(13), IN routeNo varchar(10), IN street varchar(50), IN apt varchar(10),  IN neigh varchar(50), IN city_name varchar(50), IN state_name varchar(2), IN zip varchar(10)) BEGIN INSERT INTO Recipients (lastname, firstname, gender, home_phone, cell_phone, route_no) VALUES (lastName, firstName, sex, h_phone, c_phone, routeNo); INSERT INTO Recip_Address (street_address, apt_no, neighborhood, city, state, zip_code, main_id) VALUES (street, apt, neigh, city_name, state_name, zip, Last_Insert_ID()); END;";
 
-if (!$mysqli->query("CREATE PROCEDURE <procedurename>(IN <value>) BEGIN INSERT INTO tablename(<fieldname>) VALUES(<value from IN above); END;")) 
+if (!$conn->query($createrecipAndAddress))
 {
-    echo "Stored procedure creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    echo "Stored procedure creation of pop_recipAndAddress failed: (" . $conn->errno . ") " . $conn->error;
 } else {
-    echo "Stored procedure created sucessfully!");
+    echo "Stored procedure created pop_recipAndAddress sucessfully!";
 }
-
 
 ?>
