e87612bf21c2c21bc652481d7e34da44a443c609
Brian <brian.mcgonagill@tylertech.com>
2015-07-18 11:45:23 -0500

    updated initial stored procedure
    Update the initial stored procedure for inserting recipient data into
    the database to include Recip info, Recipient Address info, and
    Recipient Gift info.

--- ssideScripts/CreateStoredProcs.php
+++ ssideScripts/CreateStoredProcs.php
@@ -4,7 +4,7 @@
 // This file is to create stored procedures for the BASTAS application
 // ************************************************************************
 
-$ini_array = parse_ini_file("php/src_files/config/config.ini.php");
+$ini_array = parse_ini_file("php/src_files/config/config.php");
 
 $ServerAddress = $ini_array['ServerName'];
 $Username = $ini_array['Username'];
@@ -21,7 +21,7 @@ if ($conn->connect_error)
     echo "<br /><br />";
 }
 
-$createrecipAndAddress = "CREATE PROCEDURE pop_recipAndAddress(IN lastName varchar(30), IN firstName varchar(30), IN sex varchar(1), IN h_phone varchar(13), IN c_phone varchar(13), IN routeNo varchar(10), IN street varchar(50), IN apt varchar(10),  IN neigh varchar(50), IN city_name varchar(50), IN state_name varchar(2), IN zip varchar(10)) BEGIN INSERT INTO Recipients (lastname, firstname, gender, home_phone, cell_phone, route_no) VALUES (lastName, firstName, sex, h_phone, c_phone, routeNo); INSERT INTO Recip_Address (street_address, apt_no, neighborhood, city, state, zip_code, main_id) VALUES (street, apt, neigh, city_name, state_name, zip, Last_Insert_ID()); END;";
+$createrecipAndAddress = "CREATE DEFINER=`bmcgonag`@`localhost` PROCEDURE `pop_recipAndAddress`(IN lastName varchar(30), IN firstName varchar(30), IN sex varchar(1), IN h_phone varchar(13), IN c_phone varchar(13), IN routeNo varchar(10), IN street varchar(50), IN apt varchar(10),  IN neigh varchar(50), IN city_name varchar(50), IN state_name varchar(2), IN zip varchar(10),IN giftNo int(11),IN giftDesc varchar(500),IN giftSize varchar(20)) BEGIN DECLARE id INT DEFAULT 0; INSERT INTO Recipients (lastname, firstname, gender, home_phone, cell_phone, route_no) VALUES (lastName, firstName, sex, h_phone, c_phone, routeNo); SET mainId = last_insert_id();INSERT INTO Recip_Address (street_address, apt_no, neighborhood, city, state, zip_code, main_id) VALUES (street, apt, neigh, city_name, state_name, zip, mainId); INSERT INTO Gifts (gift_no, description, size, main_id) VALUES (giftNo, giftDesc, giftSize, mainId);END;";
 
 if (!$conn->query($createrecipAndAddress))
 {
