cd2e5cf0f5d810d2f37aa69c10e73dc00edfb87f
Brian <brian.mcgonagill@tylertech.com>
2015-08-27 19:43:15 -0500

    Updates to html and php
    updates to html section, and updates to make admin initial install work.

--- ssideScripts/addAdmin.php
+++ ssideScripts/addAdmin.php
@@ -3,7 +3,35 @@
     // this script will run the stored procedure to
     // create an admin user
 
+$ini_array = parse_ini_file("php/src_files/config/config.php");
 
+$ServerAddress = $ini_array['ServerName'];
+$Username = $ini_array['Username'];
+$Password = $ini_array['Password'];
+$Database = $ini_array['Database'];
+
+$conn = new mysqli($ServerAddress, $Username, $Password, $Database);
+
+if ($conn->connect_error) 
+{ 
+    die("Connection failed: " . $conn->connect_error); 
+} else { 
+    echo "Connected successfully"; 
+    echo "<br /><br />";
+}
+
+$admin_user_name = $_POST['admin_user_name'];
+$admin_last_input = $_POST['admin_last_input'];
+$admin_first_input = $_POST['admin_first_input'];
+$admin_email_input = $_POST['admin_email_input'];
+$admin_company_input = $_POST['admin_company_input'];
+$admin_password_input = $_POST['admin_password_input'];
+
+if (!$conn->query("CALL createAdminUser($admin_last_input, $admin_first_input, $admin_company_input, $admin_user_name, $admin_password_input, $admin_email_input)")) {
+    echo "CALL createAdminUser failed: (" . $conn->errno . ") " . $conn->error;
+} else {
+    echo "Admin Created Successfully!";
+}
 
 
 
