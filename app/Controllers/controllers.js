7db6b5b6139a041ef5ff7757c1487b42daad40f4
Brian McGonagill <bmcgonag@gmail.com>
2015-11-07 13:39:05 -0600

    Fix the issue with route not showing on Recipient
    Updated to get route to show on Recipient edit view.

--- app/Controllers/controllers.js
+++ app/Controllers/controllers.js
@@ -157,7 +157,7 @@ angular.module('Bastas.Controllers')
                 $scope.personInfo.homePhone = personInfo.home_phone;
                 $scope.personInfo.cellPhone = personInfo.cell_phone;
                 $scope.personInfo.gender = personInfo.gender;
-                $scope.personInfo.routeNo = personInfo.route_no;
+                $scope.personInfo.route = personInfo.route_no;
 
                 $scope.address = {};
                 $scope.address.addressId = personInfo.recip_address_id
